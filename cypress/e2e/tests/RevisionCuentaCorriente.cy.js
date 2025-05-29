70da14901633f0e93ed189eeb6485ab0caf05943
FcoJavierRey <frey.inerza.com>
2025-05-29 09:15:29 +0200

    Test Revision Cuenta Corriente

--- cypress/e2e/tests/RevisionCuentaCorriente.cy.js
+++ cypress/e2e/tests/RevisionCuentaCorriente.cy.js
@@ -7,46 +7,36 @@ import { Logger } from "../util/logger";
 
 describe(NominillasInterData.testSuites.RevisionNominillas, () => {
   it("Seleccionar Consulta y actualización de Datos", () => {
-   Logger.stepNumber(1);
+    Logger.stepNumber(1);
     Logger.step("Navegar a la página de inicio");
     NominillasInterMethods.navigateToNominillasInter();
-    
-    Logger.stepNumber(2);
-    Logger.step("Ingresar un nombre de usuario autorizado");
-    LoginMethods.insertUsername(LoginData.validCredentials.username);
-    LoginMethods.insertPassword(LoginData.validCredentials.password);
 
-    Logger.stepNumber(3);
-    Logger.step('Hacer clic en "Conectar" para iniciar sesión');
-    LoginMethods.clickOnLoginButton();
+    Logger.stepNumber(2);
+    Logger.step("Iniciar Sesión en NominillasInter");
+    LoginMethods.login(LoginData.validCredentials.username, LoginData.validCredentials.password )
     Logger.verification("Verificar que se muestra la página de Consulta de Nóminas e IRPF");
     LoginMethods.verifyUser();
 
-    Logger.stepNumber(4);
+    Logger.stepNumber(3);
     Logger.step("Hacer clic en enlace Consulta y actualización de datos");
     NominillasInterMethods.clicOnActualizaDatos();
     Logger.verification("Verificar que se muestra la página de Consulta y Actualización de datos");
     NominillasInterMethods.navigateToRevisionOk()
   });
 
-  
+
   it("Seleccionar Modelo de solicitud de cambio de IBAN", () => {
-   Logger.stepNumber(1);
+    Logger.stepNumber(1);
     Logger.step("Navegar a la página de inicio");
     NominillasInterMethods.navigateToNominillasInter();
     
     Logger.stepNumber(2);
-    Logger.step("Ingresar un nombre de usuario autorizado");
-    LoginMethods.insertUsername(LoginData.validCredentials.username);
-    LoginMethods.insertPassword(LoginData.validCredentials.password);
-
-    Logger.stepNumber(3);
-    Logger.step('Hacer clic en "Conectar" para iniciar sesión');
-    LoginMethods.clickOnLoginButton();
+    Logger.step("Iniciar Sesión en NominillasInter");
+    LoginMethods.login(LoginData.validCredentials.username, LoginData.validCredentials.password )
     Logger.verification("Verificar que se muestra la página de Consulta de Nóminas e IRPF");
     LoginMethods.verifyUser();
 
-    Logger.stepNumber(4);
+    Logger.stepNumber(3);
     Logger.step("Hacer clic en enlace Modelo de solicitud de cambio de IBAN");
     NominillasInterMethods.clicOnCambioIBAN();
     Logger.verification("Verificar que se muestra la página de solicitud de cambio de IBAN");
