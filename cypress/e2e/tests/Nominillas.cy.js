49f4dc0046d5c0a5b8c4c212997d920d38caee44
FcoJavierRey <frey.inerza.com>
2025-05-29 09:31:00 +0200

    Test Nominillas

--- cypress/e2e/tests/Nominillas.cy.js
+++ cypress/e2e/tests/Nominillas.cy.js
@@ -8,22 +8,17 @@ import { Logger } from "../util/logger";
 describe(NominillasInterData.testSuites.Nominillas, () => {
 
   it("Seleccionar Nominillas", () => {
-   Logger.stepNumber(1);
+    Logger.stepNumber(1);
     Logger.step("Navegar a la página de inicio");
     NominillasInterMethods.navigateToNominillasInter();
     
     Logger.stepNumber(2);
-    Logger.step("Ingresar un nombre de usuario autorizado");
-    LoginMethods.insertUsername(LoginData.validCredentials.username);
-    LoginMethods.insertPassword(LoginData.validCredentials.password);
-
-    Logger.stepNumber(3);
-    Logger.step('Hacer clic en "Conectar" para iniciar sesión');
-    LoginMethods.clickOnLoginButton();
+    Logger.step("Iniciar Sesión en NominillasInter");
+    LoginMethods.login(LoginData.validCredentials.username, LoginData.validCredentials.password);
     Logger.verification("Verificar que se muestra la página de Consulta de Nóminas e IRPF");
     LoginMethods.verifyUser();
 
-    Logger.stepNumber(4);
+    Logger.stepNumber(3);
     Logger.step("Hacer clic en botón Ver Nominilla");
     NominillasInterMethods.clicOnVerNominilla();
     Logger.verification("Verificar que se muestra la página de Nominillas");
@@ -31,28 +26,23 @@ describe(NominillasInterData.testSuites.Nominillas, () => {
   });
 
     it("Seleccionar Nominillas Anteriores", () => {
-   Logger.stepNumber(1);
+    Logger.stepNumber(1);
     Logger.step("Navegar a la página de inicio");
     NominillasInterMethods.navigateToNominillasInter();
     
     Logger.stepNumber(2);
-    Logger.step("Ingresar un nombre de usuario autorizado");
-    LoginMethods.insertUsername(LoginData.validCredentials.username);
-    LoginMethods.insertPassword(LoginData.validCredentials.password);
-
-    Logger.stepNumber(3);
-    Logger.step('Hacer clic en "Conectar" para iniciar sesión');
-    LoginMethods.clickOnLoginButton();
+    Logger.step("Iniciar Sesión en NominillasInter");
+    LoginMethods.login(LoginData.validCredentials.username, LoginData.validCredentials.password);
     Logger.verification("Verificar que se muestra la página de Consulta de Nóminas e IRPF");
     LoginMethods.verifyUser();
 
-    Logger.stepNumber(4);
+    Logger.stepNumber(3);
     Logger.step("Hacer clic en botón Seleccionar Mes");
     NominillasInterMethods.clicOnSeleccionarMes();
     Logger.verification("Verificar que se muestra la página de Nominillas Anteriores");
     NominillasInterMethods.navigateToNominillaAnteriorOk()
 
-    Logger.stepNumber(5);
+    Logger.stepNumber(4);
     Logger.step("Hacer clic en botón Ver Nominilla");
     cy.get('input[value="Ver Nominilla"]').first().click();
     Logger.verification("Verificar que se muestra la Nominilla Anterior");
